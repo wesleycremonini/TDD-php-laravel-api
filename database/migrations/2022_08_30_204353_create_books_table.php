3268067eb80513e3c135dd09ecdf3fed99ca8e0d
wesleycremonini <wesley18.cre@gmail.com>
2022-08-31 11:08:09 -0300

    update: book table

--- database/migrations/2022_08_30_204353_create_books_table.php
+++ database/migrations/2022_08_30_204353_create_books_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('isbn');
+            
             $table->timestamps();
         });
     }
