79f19b6dfb3ad15f0dfcf80b5a1e6fde9097cfd2
wesleycremonini <wesley18.cre@gmail.com>
2022-08-31 13:13:39 -0300

    update: book model fillable

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'isbn',
+    ];
 }
