46ae79e8cd35dade1dfefa75bace6afdcf2d13a9
wesleycremonini <wesley18.cre@gmail.com>
2022-08-31 13:12:52 -0300

    update: response json book controller

--- app/Http/Controllers/API/BooksController.php
+++ app/Http/Controllers/API/BooksController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Book;
 use App\Services\BooksService;
 use Illuminate\Http\Request;
 class BooksController extends Controller
@@ -14,12 +15,19 @@ class BooksController extends Controller
     public function index()
     {
         $serviceResponse = $this->service->index();
-        return $serviceResponse;
+        return response()->json($serviceResponse);
     }
 
     public function show(int $id)
     {
         $serviceResponse = $this->service->show($id);
-        return $serviceResponse;
+        return response()->json($serviceResponse);
+
+    }
+
+    public function store(Request $req)
+    {
+        $serviceResponse = $this->service->store($req->all());
+        return response()->json($serviceResponse, 201);
     }
 }
